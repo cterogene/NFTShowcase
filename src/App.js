c58a160721c309c9b109ccc1806ac691a85fb5de
Terogene Claudio <cterogene@gmail.com>
2022-11-18 11:29:13 -0500

    deployment section done with easily reusable components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import {Download, Features, SectionWrapper} from './components';
+import { Download, Features, SectionWrapper } from './components';
 import assets from './assets';
 const App = () => {
   return (
     <div>
-    <SectionWrapper
-    title="Your own store of Nifty NFTs. Start Selling & Growing"
-    description="Buy, store, collect NFTs, exchange & earn crypto.
-    Join 25 + million people using ProNef Marketplace." 
-    showBtn
-    mockupImg={assets.homeHero}
-    banner="banner"
-    />
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto.
+    Join 25 + million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
 
-<SectionWrapper
-    title="Smart User Interface Marketplace"
-    description="Experience a buttery UI of ProNef NFT Marketplace.
-    Smooth constant colors of a fluent UI design." 
-    mockupImg={assets.homeCards}
-    reverse
-    />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace.
+    Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
 
-    <Features />
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is build using Expo which runs natively al users' devices. 
+        You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+      reverse
+      />
+
+      
+
     </div>
   );
 }
